0d7a68d3d0f81aab52e3d3bf3eac4bf966190bfd
Edward Pang <53325250+epang22@users.noreply.github.com>
2020-09-08 11:15:22 -0400

    Add hexagonal symmetry

--- EMsoft_v4_0/converteuler_oim2emsoft.m
+++ EMsoft_v4_0/converteuler_oim2emsoft.m
@@ -6,8 +6,10 @@ function euler_emsoft = converteuler_oim2emsoft(euler_oim, symID, lpangles)
 % -lpangles: lattice parameters [alpha beta gamma] (deg)
 %%% Outputs:
 % -euler_emsoft: [phi1 PHI phi2] (deg) in EMsoft convention
-%%% Of non-orthogonal systems, only monoclinic b (symID=20) is implemented so far
-% 10/21/19 (Edward Pang, MIT)
+%%% Triclinic and monoclinic-c (symID=1,2) are not yet implemented
+% Original: 10/21/19 (Edward Pang, MIT)
+% Change log:
+% 9/8/20 ELP: add hexagonal symmetry
 
 
 if symID==20
@@ -16,7 +18,11 @@ if symID==20
     Rrot = axang2rotm([0 1 0 angle]);   % rotation matrixfrom OIM to EMsoft
     R = Rrot*Reuler;    % perform rotation
     euler_emsoft = om2eu(R)*180/pi;     % Euler angles (deg) in EMsoft convention
-elseif symID==1 || symID==2 || symID==3 || symID==32 || symID==6 || symID==62
+elseif symID==3 || symID==32 || symID==6 || symID==62
+	warning('I have not explicitly checked that converted Euler angles for this symmetry are correct.');
+	euler_emsoft = euler_oim;
+	euler_emsoft(1) = euler_oim(1)+30;
+elseif symID==1 || symID==2
     warning('Euler angles not explicitly converted to EMsoft convention, as this symmetry is not yet implemented. Results may be incorrect.');
     euler_emsoft = euler_oim;
 else
